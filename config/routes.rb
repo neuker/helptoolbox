772e8749c6919bc7d86a3278973b7c36d25fa053
Norm Euker <admin@njedesign.com>
2011-01-17 17:01:24 -0500

    added more stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
-Helptoolbox::Application.routes.draw do
+Helptoolbox::Application.routes.draw do |map|
   
-  resources :articles
+  # get "sessions/new"
+  #   
+  #   resources :articles, :sessions
+  #   
+  #   home '', :controller => 'articles', :action => 'index'
+  #   
+  #   match '/login', :to => 'sessions#new'
+  
+  get "sessions/new"
+
+  map.resources :articles, :sessions
+  
+  map.home '', :controller => 'articles', :action => 'index'
+  
+  # resources :sessions, :only => [:new, :create, :destroy]
+  
+  match '/login', :to => 'sessions#new'
+  
+  map.login 'login', :controller => 'sessions', :action => 'create'  
+  map.logout 'logout', :controller => 'sessions', :action => 'destroy'
   
 end
